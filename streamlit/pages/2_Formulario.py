73d18927b8af1b9b34b0e736d2664885f523e931
PaulaGonzalez29 <paula.gonzalez.eso@gmail.com>
2024-11-06 20:23:33 +0100

    Formulario

--- streamlit/pages/2_Formulario.py
+++ streamlit/pages/2_Formulario.py
@@ -98,28 +98,28 @@ def buscar_registros():
         else:
             st.warning("Por favor, ingrese un nombre para buscar")
 
-# Ejecutar funciones
-#crear_formulario()
-#buscar_registros()
+#Ejecutar funciones
+crear_formulario()
+buscar_registros()
    
         
 # --- SECCIÓN DE BÚSQUEDA DE DUEÑOS ---
-#st.header("Buscar Registros por DNI del Dueño")
-#nombre_dueño_buscar = st.text_input("Ingrese el nombre del dueño para buscar", key = "buscar_dueño")
-#if st.button("Buscar", key = "boton_buscar_dueño"):
-#    if nombre_dueño_buscar:
-#        if os.path.exists(registro_csv):
-#            # Cargamos los datos del CSV
-#            df_merged = pd.read_csv(registro_csv)
-#            # Filtrar el DataFrame para encontrar registros que coincidan con el nombre del dueño
-#            df_resultados = df_merged[df_merged['nombre_dueño']. str. contains(nombre_dueño_buscar, case = False, na = False)]
-#            # Mostrar los resultados
-#            if not df_resultados.empty:
-#                st.write("Resultados de la Búsqueda: ")
-#                st.dataframe(df_resultados)
-#            else: 
-#                st.error("No se encontraron registros para ese dueño.")
-#        else:
-#            st.warning("El archivo de registro no existe. Registre al menos un dueño y animal primero.")
-#    else: 
-#        st.warning("Por favor, ingrese un nombre para buscar")
+st.header("Buscar Registros por DNI del Dueño")
+nombre_dueño_buscar = st.text_input("Ingrese el nombre del dueño para buscar", key = "buscar_dueño")
+if st.button("Buscar", key = "boton_buscar_dueño"):
+    if nombre_dueño_buscar:
+        if os.path.exists(registro_csv):
+            # Cargamos los datos del CSV
+            df_merged = pd.read_csv(registro_csv)
+            # Filtrar el DataFrame para encontrar registros que coincidan con el nombre del dueño
+            df_resultados = df_merged[df_merged['nombre_dueño']. str. contains(nombre_dueño_buscar, case = False, na = False)]
+            # Mostrar los resultados
+            if not df_resultados.empty:
+                st.write("Resultados de la Búsqueda: ")
+                st.dataframe(df_resultados)
+            else: 
+                st.error("No se encontraron registros para ese dueño.")
+        else:
+            st.warning("El archivo de registro no existe. Registre al menos un dueño y animal primero.")
+    else: 
+        st.warning("Por favor, ingrese un nombre para buscar")
